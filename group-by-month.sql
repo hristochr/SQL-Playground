b45d9b1cc751fe23fb0796b273db0a6a720187c8
Hristo Hristov (Chris) <hthristov@mail.bg>
2017-11-23 17:12:42 +0200

    Create group-by-month.sql

---
+++ group-by-month.sql
@@ -0,0 +1,22 @@
+/*mysql syntax*/
+  SELECT col1,
+         col2,
+         col3,
+	   	CASE MONTH(col4) /*datetime column*/
+			    WHEN 1 THEN "January" 
+			    WHEN 2 THEN "February" 
+			    WHEN 3 THEN "March" 
+			    WHEN 4 THEN "April" 
+			    WHEN 5 THEN "May" 
+			    WHEN 6 THEN "June" 
+			    WHEN 7 THEN "July" 
+			    WHEN 8 THEN "August" 
+			    WHEN 9 THEN "September" 
+			    WHEN 10 THEN "October" 
+			    WHEN 11 THEN "November" 
+			    WHEN 12 THEN "December" 
+		   END AS 'Month',
+	   	ROUND(COUNT(*)/893*100,2) AS 'Percentage' /*893 - example total count of records*/
+    FROM table name
+GROUP BY MONTH(col4)  /*datetime column*/
+ORDER BY Percentage DESC
